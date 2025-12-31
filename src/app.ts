d095d53e3c0df433b6da483883d6ffa4c3f7ea4e
kamalesh psiog <kamalesh.suresh@psiog.com>
2025-12-31 12:24:12 +0530

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from "cors";
+
 import authRoutes from "./routes/auth.routes.js";
 import assetRoutes from "./routes/asset.routes.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:49892", // Angular app
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -10,3 +19,4 @@ app.use("/api/auth", authRoutes);
 app.use("/api/asset", assetRoutes);
 
 export default app;
+ 
